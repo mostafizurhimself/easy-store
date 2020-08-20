999c163dcfaf2a89bc1681c6c1f42ffb2dc8d4ac
999itsolution <999itsolution@gmail.com>
2020-08-20 12:58:43 +0600

    Service Module added

---
+++ database/migrations/2020_08_16_093740_create_service_dispatches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceDispatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_dispatches', function (Blueprint $table) {
+            $table->id();
+            $table->string('readable_id')->nullable();
+            $table->bigInteger('location_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_dispatches');
+    }
+}
