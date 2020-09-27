b43d608f9dcd2b3ba33dc9a6472b1b19becde590
999itsolution <999itsolution@gmail.com>
2020-09-27 17:54:20 +0600

    Designation redirect updated

--- app/Nova/Designation.php
+++ app/Nova/Designation.php
@@ -284,4 +284,28 @@ class Designation extends Resource
     {
         return [];
     }
+
+    /**
+     * Return the location to redirect the user after creation.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Laravel\Nova\Resource  $resource
+     * @return string
+     */
+    public static function redirectAfterCreate(NovaRequest $request, $resource)
+    {
+        return '/resources/'.static::uriKey();
+    }
+
+    /**
+     * Return the location to redirect the user after update.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Laravel\Nova\Resource  resource
+     * @return string
+     */
+    public static function redirectAfterUpdate(NovaRequest $request, $resource)
+    {
+        return '/resources/'.static::uriKey();
+    }
 }
