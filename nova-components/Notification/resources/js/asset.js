0ec803ffc03e28508ca6c734b587ba6bc96f4b6c
999itsolution <999itsolution@gmail.com>
2020-10-01 11:44:16 +0600

    Before prevent create tool moved

---
+++ nova-components/Notification/resources/js/asset.js
@@ -0,0 +1,4 @@
+// Nova Asset JS
+Nova.booting((Vue, router, store) => {
+    Vue.component('notification', require('./components/Notification'))
+  })
---
+++ nova-components/Notification/src/AssetServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Easystore\Notification;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class AssetServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('notification', __DIR__.'/../dist/js/asset.js');
+            Nova::style('notification', __DIR__.'/../dist/css/asset.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
