e25e1968797dcc2919e2d364f3f1c22f7862e32c
999itsolution <999itsolution@gmail.com>
2021-01-31 21:56:43 +0600

    Fabrics filter updated

---
+++ app/Nova/Filters/FabricCategoryFilter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use App\Models\FabricCategory;
+use Laravel\Nova\Filters\Filter;
+
+class FabricCategoryFilter extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * The displayable name of the filter.
+     *
+     * @var string
+     */
+    public $name = "Category";
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('category_id', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return FabricCategory::orderBy('name')->pluck('id', 'name');
+    }
+}
